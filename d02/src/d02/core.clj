319b4931689d17aae35c3dbe89a349373463d3e2
Michał Ciesielski <majalcmaj@gmail.com>
2019-12-06 00:40:13 +0100

    Finished day 2

--- d02/src/d02/core.clj
+++ d02/src/d02/core.clj
@@ -2,8 +2,25 @@
   (:gen-class)
   (:require [d02.intcode :as ic]))
 
+(defn run-intcode [input]
+  (first (ic/intcode input)))
+
+(defn find-code-for-number
+  ([input number] (find-code-for-number 0 0 input number))
+  ([noun verb input number]
+   (if (< noun 100)
+     (let [input-with-code (assoc input 1 noun 2 verb)
+           result (run-intcode input-with-code)]
+       (if (= result number)
+         (println "The result is " (+ (* 100 noun) verb))
+         (if (< verb 100)
+           (recur noun (inc verb) input number)
+           (recur (inc noun) 0 input number))))
+     (println "Failed to find the solution"))))
+
 (defn -main
-  "Calculating..."
   [& args]
-  (let [content (slurp "resources/input.txt")]
-    (print (ic/intcode (ic/parse-input content)))))
+  (let [input (ic/parse-input (slurp "resources/input.txt"))]
+    (if (= (first args) "d01")
+      (print (run-intcode input)))
+    (print (find-code-for-number input 19690720))))
