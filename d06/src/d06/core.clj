222bfedf91ef86c071711365aebb79f0b844ee6e
Michał Ciesielski <majalcmaj@gmail.com>
2019-12-08 22:10:23 +0100

    Day 6 ex2 done

--- d06/src/d06/core.clj
+++ d06/src/d06/core.clj
@@ -2,11 +2,16 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
+(defrecord Node [children parent])
+
+(def empty-node (Node. [] nil))
+
 (defn- add-element-to-map [curr-map [el1 el2]]
-  (let [cur-el1-val (get curr-map el1 [])
-        new-el1-val (conj cur-el1-val el2)
-        el2-val (get curr-map el2 [])]
-    (assoc curr-map el1 new-el1-val el2 el2-val)))
+  (let [cur-el1-val (get curr-map el1 empty-node)
+        new-el1-val (Node. (conj (:children cur-el1-val) el2) (:parent cur-el1-val))
+        el2-val (get curr-map el2 empty-node)
+        new-el2-val (Node. (:children el2-val) el1)]
+    (assoc curr-map el1 new-el1-val el2 new-el2-val)))
 
 (defn create-orbit-tree
   ([input] (create-orbit-tree {} input))
@@ -18,9 +23,27 @@
 
 (defn calculate-orbits-count ([tree] (calculate-orbits-count tree "COM" 0))
   ([tree object-name orbit]
-   (let [current-children (tree object-name)
+   (let [current-children (:children (tree object-name))
          children-orbits-count (map #(calculate-orbits-count tree % (inc orbit)) current-children)]
      (+ orbit (reduce + children-orbits-count)))))
+
+(defn calc-path-from-root
+  ([tree node] (calc-path-from-root tree node []))
+  ([tree node path]
+   (if node
+     (recur tree (:parent (tree node)) (conj path node))
+     (into [] (reverse path)))))
+
+(defn calc-paths-diff [[h1 & r1] [h2 & r2]]
+  (if (= h1 h2)
+    (recur r1 r2)
+    (+ (count r1) (count r2))))
+
+(defn calculate-paths-diff-you-santa [tree]
+  (let [you-path (calc-path-from-root tree "YOU")
+        santa-path (calc-path-from-root tree "SAN")
+        paths-diff (calc-paths-diff you-path santa-path)]
+    (println "You santa paths diff" paths-diff)))
 
 (defn parse-input [input]
   (mapv #(vec (str/split % #"\)")) input))
@@ -28,8 +51,11 @@
 (defn -main
   [& args]
   (with-open [rdr (clojure.java.io/reader "resources/input.txt")]
-    (let [raw-input (vec (line-seq rdr))
+    (let [arg (first args)
+          raw-input (vec (line-seq rdr))
           input (parse-input raw-input)
-          tree (create-orbit-tree input)
-          orbits-count (calculate-orbits-count tree)]
-      (println orbits-count))))
+          tree (create-orbit-tree input)]
+      (if (= arg "1")
+        (println (calculate-orbits-count tree))
+        (println "A" (calculate-paths-diff-you-santa tree))))))
+
--- d06/test/d06/core_test.clj
+++ d06/test/d06/core_test.clj
@@ -1,19 +1,19 @@
 (ns d06.core-test
   (:require [clojure.test :refer :all]
-            [d06.core :refer :all]))
-
-(def example-tree {"COM" ["B"]
-                   "B" ["C" "G"]
-                   "G" ["H"]
-                   "H" []
-                   "C" ["D"]
-                   "D" ["E" "I"]
-                   "E" ["F" "J"]
-                   "I" []
-                   "J" ["K"]
-                   "F" []
-                   "K" ["L"]
-                   "L" []})
+            [d06.core :refer :all])
+  (:import [d06.core Node]))
+(def example-tree {"COM" (Node. ["B"] nil)
+                   "B" (Node. ["C" "G"] "COM")
+                   "G" (Node. ["H"] "B")
+                   "H" (Node. [] "G")
+                   "C" (Node. ["D"] "B")
+                   "D" (Node. ["E" "I"] "C")
+                   "E" (Node. ["F" "J"] "D")
+                   "I" (Node. [] "D")
+                   "J" (Node. ["K"] "E")
+                   "F" (Node. [] "E")
+                   "K" (Node. ["L"] "J")
+                   "L" (Node. [] "K")})
 
 (deftest test-create-orbit-map
   (testing "GIVEN empty input WHEN called THEN returns empty map"
@@ -26,3 +26,13 @@
 (deftest test-calculate-orbit-count
   (testing "given tree when calculate then correct answer"
     (is (= 42 (calculate-orbits-count example-tree)))))
+
+(deftest test-calculate-path-to-root
+  (testing "when called then result correct"
+    (is (= ["COM" "B" "C" "D" "E" "J"] (calc-path-from-root example-tree "J")))))
+
+(deftest test-calculate-paths-diff
+  (testing "when called then result correct"
+    (is (= 5
+           (calc-paths-diff ["COM" "B" "C" "D" "E" "J" "K"]
+                            ["COM" "B" "G" "H"])))))
